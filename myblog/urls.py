7e694bb17f56b4fb280cc5cdacb83915d05dc6e3
islanzakhirov <islanzakhirov>
2022-02-15 01:00:11 +0500

    urls

--- myblog/urls.py
+++ myblog/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static 
-from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 from pro.views import *
 from drf_yasg import openapi
